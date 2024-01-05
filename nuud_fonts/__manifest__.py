c98c764b571d0e9cdb3586aa8e6dd1bc29d47f8e
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-01-05 20:37:29 +0530

    App: Nuud Fonts

---
+++ nuud_fonts/__manifest__.py
@@ -0,0 +1,20 @@
+
+{
+    "name": "Fonts - Nuud",
+    "version": "16.0.0.0",
+    "author": "Deepa, The Open Source Company (TOSC)",
+    "category": "Extra",
+    'website': 'https://www.tosc.nl',
+    'description': "Adds a font called 'BrownStd' to Nuud Project ",
+    "depends": ["web"],
+    "data": [
+    ],
+    'assets': {
+         'web._assets_primary_variables': [
+            "nuud_fonts/static/src/scss/primary_variables.scss",
+        ],
+    },
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+}
